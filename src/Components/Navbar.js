50ad7e4303e4bc42fc4e0c95ea0dfa23dc235bd7
Yomamajohn <yichenglau@gmail.com>
2022-09-11 15:30:53 -0700

    Add components folder

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="Johnson Lau">Johnson Lau</a>
+        <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li></li>
+        </ul>
+    </nav>
+}
